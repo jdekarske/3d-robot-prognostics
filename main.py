a309f5f5fe6c51aef994797bec942dc89c9a3a2c
Jason Dekarske <dekarskej@gmail.com>
2022-10-04 13:35:38 -0700

    add progress bar

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ A sample implementation of the degradation environment.
 
 import pandas as pd
 from degradation_env import DegradationEnv
+from tqdm import tqdm
 
 # only set in beginning
 CONTROL_FREQ = 20
@@ -12,7 +13,7 @@ HORIZON = CONTROL_FREQ * CYCLE_TIME
 
 env = DegradationEnv(
     horizon=HORIZON,
-    has_renderer=False, # set True if you want to see it rendered
+    has_renderer=True, # set True if you want to see it rendered
     logging_dir="./out/",
     logging_file="test.hdf5",  # if none uses date and time
     control_freq=CONTROL_FREQ,
@@ -20,7 +21,7 @@ env = DegradationEnv(
 
 cycles = pd.read_csv("./sampleinputs.csv", header=0)
 joints = [item for item in cycles.keys() if "joint" in item]
-for _, row in cycles.iterrows():
+for _, row in tqdm(cycles.iterrows()):
     env.label = row["label"]
     env.cycle = row["cycle"]
     env.cube_mass = row["payload"]  # kg
@@ -31,3 +32,4 @@ for _, row in cycles.iterrows():
 
     env.reset()
     env.run()
+    break
