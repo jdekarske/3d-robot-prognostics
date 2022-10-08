9e3b634662c57f34d7efd36245fdf3a376d11802
Jason Dekarske <dekarskej@gmail.com>
2022-10-08 14:30:32 -0700

    organize inputs a bit

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ from degradation_env import DegradationEnv
 CONTROL_FREQ = 20
 CYCLE_TIME = 10
 HORIZON = CONTROL_FREQ * CYCLE_TIME
+EXPFILE = "inputs/sampleinputs_Exp1.csv"
+
 
 env = DegradationEnv(
     horizon=HORIZON,
     has_renderer=False,  # set True if you want to see it rendered
     logging_dir="./out/",
-    # logging_file="test.hdf5",  # if none uses date and time
+    logging_file=EXPFILE + ".hdf5",  # if none uses date and time
     control_freq=CONTROL_FREQ,
 )
 
-cycles = pd.read_csv("./sampleinputs.csv", header=0)
+cycles = pd.read_csv(EXPFILE, header=0)
 joints = [item for item in cycles.keys() if "joint" in item]
 for _, row in tqdm(cycles.iterrows(), total=len(cycles)):
     env.label = row["label"]
